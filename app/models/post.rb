855f74a8781d90a6632e12381b72d3235ce34b9c
Blaire Baker <bakerblaire@gmail.com>
2020-12-12 14:15:08 -0600

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :must_be_clickbait
+
+    def must_be_clickbait
+        if title != nil && title.exclude?("Won't Believe") && title.exclude?("Secret") && title.exclude?("Top") && title.exclude?("Guess")
+            errors.add(:title, "Must be clickbait")
+        end
+    end
+
+
 end
